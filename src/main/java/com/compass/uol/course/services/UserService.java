0356b3857c7c0f4c1e37e024292328d96de29265
Bruno Pressi <brunopressi2012@gmail.com>
2025-03-17 14:09:41 -0300

    Service layer, component registration

---
+++ src/main/java/com/compass/uol/course/services/UserService.java
@@ -0,0 +1,27 @@
+package com.compass.uol.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.compass.uol.course.entities.User;
+import com.compass.uol.course.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+	
+}
