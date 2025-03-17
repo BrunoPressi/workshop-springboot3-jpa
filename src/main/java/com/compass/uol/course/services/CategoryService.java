aeea44c166f80aaa845ddb015b375d55635231aa
Bruno Pressi <brunopressi2012@gmail.com>
2025-03-17 16:05:28 -0300

    Category

---
+++ src/main/java/com/compass/uol/course/services/CategoryService.java
@@ -0,0 +1,28 @@
+package com.compass.uol.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.compass.uol.course.entities.Category;
+import com.compass.uol.course.entities.Order;
+import com.compass.uol.course.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+	
+	@Autowired
+	private CategoryRepository repository;
+	
+	public List<Category> findAll() {
+		return repository.findAll();
+	}
+	
+	public Category findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		return obj.get();
+	}
+	
+}
