2c51424bf86bd26b77af4827b7ac651b6ce80ac4
Bruno Pressi <brunopressi2012@gmail.com>
2025-03-18 16:43:20 -0300

    Exception handling - findById

---
+++ src/main/java/com/compass/uol/course/services/exception/ResourceNotFoundException.java
@@ -0,0 +1,11 @@
+package com.compass.uol.course.services.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public ResourceNotFoundException(Object id) {
+		super("Resource not found. ID " + id);
+	}
+
+}
