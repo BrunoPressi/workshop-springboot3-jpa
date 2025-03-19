549396cb07f11dc4dd0673fc00b387114347d5a1
Bruno Pressi <brunopressi2012@gmail.com>
2025-03-19 12:56:45 -0300

    Exception handling - update

--- src/main/java/com/compass/uol/course/services/UserService.java
+++ src/main/java/com/compass/uol/course/services/UserService.java
@@ -12,39 +12,47 @@ import com.compass.uol.course.repositories.UserRepository;
 import com.compass.uol.course.services.exception.DatabaseException;
 import com.compass.uol.course.services.exception.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository repository;
-	
+
 	public List<User> findAll() {
 		return repository.findAll();
 	}
-	
+
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
-	
+
 	public User insert(User obj) {
 		return repository.save(obj);
 	}
-	
+
 	public void delete(Long id) {
 		try {
 			if (!repository.existsById(id)) {
-		        throw new ResourceNotFoundException(id);
-		    }
-		    repository.deleteById(id);
+				throw new ResourceNotFoundException(id);
+			}
+			repository.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
 		}
-	    catch (DataIntegrityViolationException e) {
-	    	throw new DatabaseException(e.getMessage());
-	    }
 	}
+
 	public User update(Long id, User obj) {
-		User entity = repository.getReferenceById(id);
-		updateData(entity, obj);
+		User entity = null;
+		try {
+			entity = repository.getReferenceById(id);
+			updateData(entity, obj);
+		}
+		catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 		return repository.save(entity);
 	}
 
@@ -53,5 +61,5 @@ public class UserService {
 		entity.setEmail(obj.getEmail());
 		entity.setFone(obj.getFone());
 	}
-	
+
 }
