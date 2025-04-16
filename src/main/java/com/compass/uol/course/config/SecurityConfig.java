8f5686f840ac87fd1d549d40bda79b13510eda13
Bruno Pressi <brunopressi2012@gmail.com>
2025-04-16 15:03:58 -0300

    feat: add url authorization

--- src/main/java/com/compass/uol/course/config/SecurityConfig.java
+++ src/main/java/com/compass/uol/course/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.compass.uol.course.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.core.userdetails.User;
@@ -10,26 +11,34 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-	
-    @Bean
-    UserDetailsService userDetailsService() {
-        UserDetails user = User
-            .withUsername("username")
-            .password(passwordEncoder().encode("123"))
-            .roles("USER")
-            .build();
-        return new InMemoryUserDetailsManager(user);
-    }
-	
-	/*@Bean
+
+	@Bean
+	UserDetailsService userDetailsService() {
+		UserDetails user = User.withUsername("username").password(passwordEncoder().encode("123")).roles("USER")
+				.build();
+		return new InMemoryUserDetailsManager(user); // Cria um usuário na memória
+	}
+
+	@Bean
 	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
+	    http
+        .csrf(csrf -> csrf.disable()) // Desativa CSRF
+        .authorizeHttpRequests(authz -> authz
+            .requestMatchers("/products/**").authenticated()  // Exige autenticação para /products/**
+            .anyRequest()/*.permitAll()*/)  // Permite acesso a outras rotas
+        .formLogin(form -> form
+            //.loginPage("/login") // Página personalizada de login
+            .permitAll());  // Permite o acesso à página de login sem autenticação
 
-	}*/
+	    return http.build();
+
+	}
 
 	@Bean
 	PasswordEncoder passwordEncoder() {
