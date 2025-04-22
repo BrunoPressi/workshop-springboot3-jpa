43a3ebecae885f53dd612ed2eda7758438015d20
Bruno Pressi <brunopressi2012@gmail.com>
2025-04-22 13:44:54 -0300

    feat: configuring security in Swagger

--- src/main/java/com/compass/uol/course/config/SwaggerConfig.java
+++ src/main/java/com/compass/uol/course/config/SwaggerConfig.java
@@ -1,10 +1,14 @@
 package com.compass.uol.course.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+
 
 @Configuration
 public class SwaggerConfig {
@@ -21,6 +25,15 @@ public class SwaggerConfig {
                     .url("https://github.com/BrunoPressi")
                     .email("BrunoPressi2012@gmail.com")
                 )
+            )
+            .addSecurityItem(new SecurityRequirement().addList("JWT"))
+            .components(new io.swagger.v3.oas.models.Components()
+                .addSecuritySchemes("JWT", new SecurityScheme()
+                    .type(SecurityScheme.Type.APIKEY)
+                    .in(SecurityScheme.In.HEADER)
+                    .name("Authorization")
+                )
             );
     }
+    
 }
